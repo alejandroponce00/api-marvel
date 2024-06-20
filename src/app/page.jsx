e9e6f67a411ad9cf64a6b778a3be047cd9f12dd2
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-06-20 20:15:48 -0300

    correccion body

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,44 +1,54 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import md5 from 'md5';
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import md5 from "md5";
 
 function Home() {
   const [personajes, setPersonajes] = useState([]);
 
   useEffect(() => {
-    const ts = '1';
-    const privateKey = '9918f218ea7375995809a8da4f285f9543203e4a';
-    const publicKey = '68f80bf784fe016d66c599ac79bd30ff';
+    const ts = "1";
+    const privateKey = "9918f218ea7375995809a8da4f285f9543203e4a";
+    const publicKey = "68f80bf784fe016d66c599ac79bd30ff";
     const hash = md5(ts + privateKey + publicKey);
 
-    axios.get(`https://gateway.marvel.com:443/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-      .then(res => {
-        setPersonajes(res.data.data.results);  // Accediendo correctamente a los resultados
-     console.log(res.data) })
-      .catch(error => console.log(error));
+    axios
+      .get(
+        `https://gateway.marvel.com:443/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+      )
+      .then((res) => {
+        setPersonajes(res.data.data.results); // Accediendo correctamente a los resultados
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   return (
-    <div  >
-      <h1 className="text-center text-4xl text-red-400">Marvel</h1>
-      <div className='    columns-3'>
-      {personajes.map(per => (
-        <div className=" container m-6 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 " key={per.id}>
-          <div className="rounded container" >
-            <img 
-              src={`${per.thumbnail.path}.${per.thumbnail.extension}`} 
-              className="card-img-top" 
-              alt={per.name} 
-            />
-            
+    <div className="mx-auto">
+      <h1 className="text-center text-6xl text-red-800 my-8">Marvel Api</h1>
+      <div className=" container   columns-3 p-4">
+        {personajes.map((per) => (
+          <div
+            className=" container m-6  max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
+            key={per.id}
+          >
+              <p className=" text-slate-300  text-center">
+                <strong>Titulo </strong>:<br /> {per.title}
+              </p>
+            <div className="rounded container">
+              <img
+                src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
+                className="card-img-top"
+                alt={per.name}
+              />
+            </div>
+           
               
+            </div>
           
-          </div><div className='border-red-700'><p className=" text-red-700  content-end">Id:{per.id}</p>
-          <p className=" text-slate-300  content-end">Titulo:{per.title}</p>
-        </div></div>
-      ))}
-    </div></div>
+        ))}
+      </div>
+    </div>
   );
 }
 
