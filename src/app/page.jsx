6c2d98e3f133d6d25320934b4780a74d58fd7db8
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-06-20 19:47:15 -0300

    agregado de cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,30 +12,31 @@ function Home() {
     const publicKey = '68f80bf784fe016d66c599ac79bd30ff';
     const hash = md5(ts + privateKey + publicKey);
 
-    axios.get(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+    axios.get(`https://gateway.marvel.com:443/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
       .then(res => {
         setPersonajes(res.data.data.results);  // Accediendo correctamente a los resultados
-      })
+     console.log(res.data) })
       .catch(error => console.log(error));
   }, []);
 
   return (
     <div  >
       <h1 className="text-center text-4xl text-red-400">Marvel</h1>
-      <div className='  grid-cols-3  columns-3'>
+      <div className='    columns-3'>
       {personajes.map(per => (
-        <div className="p-10 " key={per.id}>
-          <div className="rounded container" style={{ width: "5rem", height: "5rem" }}>
+        <div className=" container m-6 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 " key={per.id}>
+          <div className="rounded container" >
             <img 
               src={`${per.thumbnail.path}.${per.thumbnail.extension}`} 
               className="card-img-top" 
               alt={per.name} 
             />
             
-              <p className=" text-slate-300 text-xl content-end">{per.name}</p>
+              
           
-          </div>
-        </div>
+          </div><div className='border-red-700'><p className=" text-red-700  content-end">Id:{per.id}</p>
+          <p className=" text-slate-300  content-end">Titulo:{per.title}</p>
+        </div></div>
       ))}
     </div></div>
   );
